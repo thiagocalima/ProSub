ea01ccaa3b5dbf30f3bd95ef3f511b9bc11b56d0
Thiago Lima <thiagocalima@gmail.com>
2013-05-26 05:53:48 -0300

    Atualizando a classe ListaAlocacoesPendentesService

--- src/Service/ListaAlocacoesPendentesService.java
+++ src/Service/ListaAlocacoesPendentesService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  *
- * @author Thiago
+ * @author Thiago Lima
  */
 public class ListaAlocacoesPendentesService {
     private List<AusenciaModel> listaDeAusencias = null;
