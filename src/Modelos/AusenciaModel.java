625cec48e701c2c59f86b8a5a0314b0f0e9754bd
Thiago Lima <thiagocalima@gmail.com>
2013-05-26 05:33:08 -0300

    Criação do modelo de ausencia

---
+++ src/Modelos/AusenciaModel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+import Dominio.Professor;
+
+/**
+ *
+ * @author Thiago
+ */
+public class AusenciaModel {
+    public Professor professorAusente;
+    public Long id;
+}
